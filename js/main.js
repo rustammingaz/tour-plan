d34a57b80f734b7a31cbef82697f05c00e1c184b
rustammingaz <rustammingaz@gmail.com>
2020-08-03 14:44:48 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-var mySwiper = new Swiper(".swiper-container", {
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+var hotelSlider = new Swiper(".hotel-slider", {
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport: false,
+  // },
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   effect: "coverflow",
+});
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport: false,
+  // },
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
 });
